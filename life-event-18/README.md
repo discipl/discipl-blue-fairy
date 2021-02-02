d82c145e0dffa293aa924f11776f1b949b9cf230
Bas Kaptijn <Bas.Kaptijn@ictu.nl>
2021-02-02 11:01:10 +0100

    update

---
+++ life-event-18/README.md
@@ -0,0 +1,9 @@
+how to run:
+
+in terminal 1:
+cd actionserver
+node actionserver.js
+
+in terminal 2:
+rasa train
+rasa shell --endpoints endpoints.yml
--- life-event-18/actionserver/actionserver.js
+++ life-event-18/actionserver/actionserver.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var app = express();
+const bodyParser = require("body-parser");
 
-// This responds with "Hello World" on the homepage
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//var lawReg = require('discipl-law-reg')
+//import Util from '../src/utils/util'
+//const util = new Util(lawReg)
+//var lifeEventChangeFlint = require('life-event-age.flint')
+//util.setupModel(lifeEventAgeFlint)
+
 app.get('/', function (req, res) {
    res.send('{"flintrasa":"1.0"}');
 })
 
 // This responds a POST request for the homepage
 app.post('/webhook', function (req, res) {
-   console.log("Got a POST request");
-   res.send('Hello POST');
+
+   console.log(req.body);
+   // update intent
+   // get possible actions
+   // take action (if there is unique intent to)
+   // update rasa model
+   // set response to user
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify({events:[{"event":"followup", "name":"utter_iamabot"}],responses:[{text:"Hello from flint rasa action server"}]}));
 })
 
 var server = app.listen(5055, 'localhost', function () {
